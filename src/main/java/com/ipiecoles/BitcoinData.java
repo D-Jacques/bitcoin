6edd9616d1ecb2f3dcdd75fe5c37298f4451e64b
DUCROUX <jacques.ducroux@gmail.com>
2021-02-11 10:13:38 +0100

    Initialisation project bitcoin

---
+++ src/main/java/com/ipiecoles/BitcoinData.java
@@ -0,0 +1,56 @@
+package com.ipiecoles;
+
+import java.util.List;
+import java.util.Objects;
+
+public class BitcoinData {
+    private Double bitcoinAmount;
+    private List<String> currencyList;
+
+    public BitcoinData() {
+    }
+
+    public BitcoinData(Double bitcoinAmount, List<String> currencyList) {
+        this.bitcoinAmount = bitcoinAmount;
+        this.currencyList = currencyList;
+    }
+
+    public Double getBitcoinAmount() {
+        return bitcoinAmount;
+    }
+
+    public void setBitcoinAmount(Double bitcoinAmount) {
+        this.bitcoinAmount = bitcoinAmount;
+    }
+
+    public List<String> getCurrencyList() {
+        return currencyList;
+    }
+
+    public void setCurrencyList(List<String> currencyList) {
+        this.currencyList = currencyList;
+    }
+
+    @Override
+    public String toString() {
+        return "BitcoinData{" +
+                "bitcoinAmount=" + bitcoinAmount +
+                ", currencyList=" + currencyList +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        BitcoinData that = (BitcoinData) o;
+        return bitcoinAmount.equals(that.bitcoinAmount) &&
+                currencyList.equals(that.currencyList);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(bitcoinAmount, currencyList);
+    }
+
+}
---
+++ src/test/java/com/ipiecoles/BitcoinTest.java
@@ -0,0 +1,36 @@
+package com.ipiecoles;
+
+import org.junit.Test;
+
+public class BitcoinTest {
+
+    @Test
+    public void testBitcoin(){
+        //Given
+        Bitcoin bitcoin = new Bitcoin();
+
+        //When
+        bitcoin.getBitcoin();
+
+        //Then
+
+
+//        {
+//            "bitcoinAmount": 3,
+//                "currencyList": "EUR,USD"
+//        }
+
+
+//        {
+//            "bitcoinAmount":3.0,
+//                "currenciesEquivalent":{
+//                    "EUR":8962.74,
+//                    "USD":10225.14
+//        }
+//        }
+
+//        Assertions.assertThat(bitcoins.getBitcoins()).isEqualTo( "bitcoinAmount":3.0, "currenciesEquivalent":{"EUR":8962.74, "USD":10225.14})
+
+    }
+
+}
